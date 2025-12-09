58b0ec2604b975f58d6d74896909bec40249350b
Ricardo Saumeth <ricardosantana46@hotmail.com>
2025-12-09 16:04:19 +0000

    rollback changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import { useEffect } from "react"
-import { Provider, useDispatch } from "react-redux"
-import getStore, { type AppDispatch } from "@modules/app/redux/store"
-import { bootstrapApp } from "@modules/app/slice"
-
-const store = getStore()
-
-const AppContent = () => {
-  const dispatch = useDispatch<AppDispatch>()
-
-  useEffect(() => {
-    dispatch(bootstrapApp())
-  }, [])
-
-  return <h1>hello..</h1>
-}
-
 function App() {
-  return <h1>Cryto App</h1>
+  return <h1>Crypto App</h1>
 }
 
 export default App
+
