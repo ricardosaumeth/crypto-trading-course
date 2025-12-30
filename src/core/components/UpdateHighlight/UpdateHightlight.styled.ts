ae95291897e5e869692c4a8c55b5e22dfc7012fa
Ricardo Saumeth <ricardosantana46@hotmail.com>
2025-12-30 12:58:44 +0000

    feat: add market component

---
+++ src/core/components/UpdateHighlight/UpdateHightlight.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Content = styled.div`
+  display: flex;
+`
+
+export const IdenticalPart = styled.div``
+
+export const DigitContainer = styled.span`
+  display: inline-block;
+`
---
+++ src/modules/tickers/components/Market/Market.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import Palette from "@theme/style"
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .ag-cell {
+    font-size: 12px;
+  }
+
+  .selected-row {
+    font-weight: 700;
+    background-color: ${Palette.BackgroundColorOnHover};
+  }
+`
