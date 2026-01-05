9997a3e9835be523316b01877cdb6bf42d5e71cf
Ricardo Saumeth <ricardosantana46@hotmail.com>
2026-01-05 11:38:45 +0000

    feat: add tickers component

---
+++ src/modules/tickers/components/Ticker/Ticker.tsx
@@ -0,0 +1,44 @@
+import { Container, CurrencyPair, Price, RelativeChange, Change } from "./Ticker.styled"
+import UpdateHighlight from "@core/components/UpdateHighlight/UpdateHighlight"
+import { formatCurrencyPair, formatPrice } from "@modules/refence-data/utils"
+import TrendIndicator from "@core/components/TrenIndicator"
+
+export interface Props {
+  currencyPair: string
+  lastPrice: number
+  dailyChange: number
+  dailyChangeRelative: number
+  onClick: () => void
+  isActive: boolean
+}
+
+const Ticker = ({
+  currencyPair,
+  lastPrice,
+  dailyChange,
+  dailyChangeRelative,
+  onClick,
+  isActive,
+}: Props) => {
+  const isPositiveChange = dailyChange > 0
+  const percentChange = dailyChangeRelative ? dailyChangeRelative * 100 : undefined
+
+  return (
+    <Container onClick={onClick} $isActive={!!isActive}>
+      <CurrencyPair>{formatCurrencyPair(currencyPair)}</CurrencyPair>
+      <Price>
+        <UpdateHighlight value={formatPrice(lastPrice)} effect="zoom" />
+      </Price>
+      <RelativeChange $isPositive={isPositiveChange}>
+        <TrendIndicator value={dailyChangeRelative} />
+        <UpdateHighlight value={percentChange?.toFixed(2)} />
+        {percentChange && "%"}
+      </RelativeChange>
+      <Change $isPositive={isPositiveChange}>
+        <UpdateHighlight value={dailyChange?.toFixed(2)} />
+      </Change>
+    </Container>
+  )
+}
+
+export default Ticker
---
+++ src/modules/tickers/components/Tickers/Tickers.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+import { usePrevious } from "@core/hooks/usePrevious"
+import { Container, TickerWrapper, type ScrollDirection } from "./Tickers.styled"
+import Ticker from "../Ticker"
+
+export interface Props {
+  currencyPairs: string[]
+  selectedCurrencyPairIndex?: number
+}
+
+const Tickers = ({ currencyPairs, selectedCurrencyPairIndex }: Props) => {
+  const [direction, setDirection] = useState<ScrollDirection>("left")
+  const previousSelectedCurrencyPairIndex = usePrevious(selectedCurrencyPairIndex)
+
+  useEffect(() => {
+    const direction =
+      Number(previousSelectedCurrencyPairIndex || 0) > Number(selectedCurrencyPairIndex || 0)
+        ? "right"
+        : "left"
+    setDirection(direction)
+  }, [selectedCurrencyPairIndex])
+
+  return (
+    <Container $itemCount={currencyPairs.length} className="tickers">
+      {currencyPairs.map((currencyPair, index) => (
+        <TickerWrapper
+          $index={index}
+          $itemCount={currencyPairs.length}
+          $direction={direction}
+          key={currencyPair}
+        >
+          <Ticker currencyPair={currencyPair} />
+        </TickerWrapper>
+      ))}
+    </Container>
+  )
+}
+
+export default Tickers
